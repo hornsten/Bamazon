38805786e929d4ebc66b0a6cb6eed5c40c8f3ebd
Jessica Hornsten <hornsten@users.noreply.github.com>
2017-01-13 13:08:27 -0600

    Add default values to total_sales and product_sales

--- database-schema.sql
+++ database-schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE products (
 item_id INT auto_increment NOT NULL,
 product_name VARCHAR(250) NULL,
 department_name VARCHAR(250) NULL,
-price DECIMAL(10,2) NOT NULL,
+price DECIMAL(10,2) NOT NULL DEFAULT 0,
 stock_quantity INT NULL,
 PRIMARY KEY (item_id)
 );
@@ -15,10 +15,5 @@ CREATE TABLE departments (
 department_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY_KEY,
 department_name VARCHAR(250) NOT NULL,
 over_head_costs DECIMAL(10,2),
-total_sales DECIMAL(10,2)
+total_sales DECIMAL(10,2) DEFAULT 0
 );
-
-INSERT INTO departments VALUES 
-(NULL, 'Shoes', 88.12, 0.00),
-(NULL, 'Swimwear', 12.12, 0.00);
-
